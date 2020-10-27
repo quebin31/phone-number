d54ddff31f8512a54dfc1c14539138e626075438
Kevin Del Castillo <quebin31@gmail.com>
2020-10-26 19:19:40 -0500

    fix: remove `extract_digits` in favor of using iterators to build the final String.

--- src/lib.rs
+++ src/lib.rs
@@ -7,34 +7,28 @@ fn valid_format(string: &str) -> bool {
     regex.is_match(string)
 }
 
-fn extract_digits(string: &str) -> Vec<u8> {
-    string
-        .chars()
-        .filter_map(|c| {
-            if c.is_ascii_digit() {
-                c.to_digit(10).map(|d| d as u8)
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
 pub fn number(user_number: &str) -> Option<String> {
     let user_number = user_number.trim();
 
     if !valid_format(user_number) {
         None
     } else {
-        let digits = extract_digits(user_number);
-        let digits_len = digits.len();
-
-        let cleaned = digits
-            .into_iter()
-            .skip(digits_len - 10)
+        let mut digits: String = user_number
+            .chars()
+            .filter_map(|c| {
+                if c.is_ascii_digit() {
+                    c.to_digit(10).map(|d| d as u8)
+                } else {
+                    None
+                }
+            })
             .map(|d| (d + 48) as char)
             .collect();
 
-        Some(cleaned)
+        if digits.len() == 11 {
+            digits.remove(0);
+        }
+
+        Some(digits)
     }
 }
